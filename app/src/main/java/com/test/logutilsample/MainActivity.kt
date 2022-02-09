14cb8e82b5252d9efcafa3a458ca7f23342a208c
deshmukhmayuresh21 <mayuresh.deshmukh@vivant.me>
2022-02-09 10:51:33 +0530

    v1.2

--- app/src/main/java/com/test/logutilsample/MainActivity.kt
+++ app/src/main/java/com/test/logutilsample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.test.logutilsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.test.logutil.LogUtil
+import com.test.util.Utility
 import com.test.utility.DateHelper
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         LogUtil.logD("Hi Mayuresh Deshmukh ")
         LogUtil.logD(""+DateHelper.currentDateAsStringddMMMyyyy)
+        Utility.Util.printOwnerName()
     }
 }
