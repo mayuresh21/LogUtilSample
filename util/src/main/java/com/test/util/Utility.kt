9f3590bcbfe8539a3118b173c6eb9184b185a10d
deshmukhmayuresh21 <mayuresh.deshmukh@vivant.me>
2022-02-09 09:27:17 +0530

    v1.2

---
+++ util/src/main/java/com/test/util/Utility.kt
@@ -0,0 +1,12 @@
+package com.test.util
+
+import android.util.Log
+
+class Utility {
+
+    object Util {
+        fun printOwnerName(){
+            Log.d("LibSample=> ","Mayuresh Deshmukh")
+        }
+    }
+}
